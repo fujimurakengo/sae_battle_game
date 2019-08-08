6a9398dd3e6aec21b0af519aa9faafec8ee83285
fujimurakengo <naruawaryousuke@gmail.com>
2019-08-08 19:26:33 +0900

    邪魔な奴を削除

--- kaisen_game/kaisen_game.ino
+++ kaisen_game/kaisen_game.ino
@@ -1,22 +1,29 @@
+/*
+0 means Player A
+1 means Player B
 
+*/
+
+#include <Servo.h>
 #include <Adafruit_NeoPixel.h>
-#define a0 A_player_contlor_x
-#define a1 A_player_contlor_y
-#define a2 B_player_contlor_x
-#define a3 B_player_contlor_y
+#include <Pin_init.h>
+
+int *A_player_pos[2] ={0,0};
+int *B_player_pos[2] ={0,0};
+int Player_turn=1;
+
+int Countown_Timer=0;
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(LED_Pin_OUT, LED_Pin_IN, NEO_GRB + NEO_KHZ800);
 
 
-#define 8  a_led
-#define 9  b_led
-#define 10 c_led
-#define 11 d_led
-#define 12 e_led
-#define 13 f_led
-#define 28 g_led
-
-
-
-
+void select_map_possion(int x, int y,int **pos){
+  int x_axis_pos = int(analogRead(x) * 4 /1023);
+  int y_axis_pos = int(analogRead(y) * 4 /1023);
+  //here.you use pointer to detect axises
+  
+  
+}
+  
 
 int A_plyer_map[4][4]={{0,0,0,0},
                        {0,0,0,0},
@@ -29,15 +36,17 @@ int B_plyer_map[4][4]={{0,0,0,0},
                        {0,0,0,0},
                        {0,0,0,0}
                        };
+                       
 Servo water_direction;  // create servo object to control a servo 
 Servo water_swich;  // create servo object to control a servo 
 
-
 void setup() {
   // put your setup code here, to run once:
-  water_direction.attach(4); 
-  water_direction.attach(5); 
-  
+  water_direction.attach(4); // This initializes serv motor
+  water_direction.attach(5); // This initializes serv motor
+  pixels.begin(); // This initializes the NeoPixel library.
+
+//player contlloer GPIO setting
   pinMode(A_player_contlor_x,INPUT);
   pinMode(A_player_contlor_y,INPUT);  
   pinMode(B_player_contlor_x,INPUT);
@@ -51,12 +60,26 @@ void setup() {
   pinMode(e_led,OUTPUT);
   pinMode(f_led,OUTPUT);
   pinMode(g_led,OUTPUT);
-
-
-
+  
+ //setup sequence
+ 
+  Countown_Timer = 10;
+  while(Countown_Timer){
+    //select_map_possion(A_player_contlor_x, A_player_contlor_y, &A_player_pos);
+   // select_map_possion(B_player_contlor_x, B_player_contlor_y, &B_player_pos);
+  }
 }
 
+
+
 void loop() {
-  // put your main code here, to run repeatedly: 
-  
+  // put your main code here, to run repeatedly:
+    Player_turn = (1+Player_turn) % 2;
+    Countown_Timer = 10; 
+    
+    while(Countown_Timer){
+
+    }
+    
+    
 }
