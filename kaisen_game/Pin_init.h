c4cd2e72c12a90ee5b11ea8f103ec8872db4cd2b
fujimurakengo <naruawaryousuke@gmail.com>
2019-08-08 19:26:55 +0900

    マクロをヘッダに追加

---
+++ kaisen_game/Pin_init.h
@@ -0,0 +1,17 @@
+#define  A_player_contlor_x 0
+#define  A_player_contlor_y 1
+#define  B_player_contlor_x 2
+#define  B_player_contlor_y 3
+#define LED_Pin_IN   6
+#define LED_Pin_OUT  7
+
+#define  a_led  8
+#define  b_led  9
+#define  c_led 10
+#define  d_led 11
+#define  e_led 12
+#define  f_led 13
+#define  g_led 14
+
+
+
